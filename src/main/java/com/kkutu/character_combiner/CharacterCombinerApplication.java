4ffe2ef0a65208af0b5dcf68185d391fe8ccc659
chanwoo7 <chanwoo9987@gmail.com>
2025-02-15 23:40:26 +0900

    Initialize Project

---
+++ src/main/java/com/kkutu/character_combiner/CharacterCombinerApplication.java
@@ -0,0 +1,13 @@
+package com.kkutu.character_combiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharacterCombinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharacterCombinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kkutu/character_combiner/CharacterCombinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkutu.character_combiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharacterCombinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
